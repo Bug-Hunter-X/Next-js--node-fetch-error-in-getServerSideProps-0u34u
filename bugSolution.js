dce4c52423e0ff10eab13df84531c8117ad9688d
BugHunter-X <askzenai@gmail.com>
2025-02-13 17:12:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,23 @@
+```javascript
+// pages/about.js
+
+export async function getServerSideProps() {
+  const response = await fetch('https://api.example.com/data');
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function About({ data }) {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+```
